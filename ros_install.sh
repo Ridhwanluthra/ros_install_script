66615b70a00c29010e0bee4345d2758ab5960973
Ridhwan Luthra <ridhwancode@gmail.com>
2018-03-19 17:07:36 +0530

    fixed typos

--- ros_install.sh
+++ ros_install.sh
@@ -27,8 +27,8 @@ if [[ -r /etc/os-release ]]; then
 	    fi
 	    if [[ $distro=="random" ]]; then
 	    	sudo sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-			sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116 -y
-			sudo apt get update -y
+			sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+			sudo apt update
 			sudo apt install ros-"$distro"-desktop-full -y
 			sudo rosdep init
 			rosdep update
